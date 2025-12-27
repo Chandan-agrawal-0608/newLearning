554fb472cca00c5b2a500c3bd630e5eeb682569a
Chandan Agrawal <Chandanagrawal91116@gmail.com>
2025-12-27 15:52:05 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,17 @@
+console.log("hello")
+console.log ("kaise ho coder bhaiya app")
+
+const balance =100;
+
+const age=20;
+
+function updatebalance(value){
+  balance+=value;
+}
+updatebalance(10);
+
+function updateage(value){
+  age+=value;
+}
+
+console.log(updateage(20));
